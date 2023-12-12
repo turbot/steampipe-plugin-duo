6d602a009229072f7d399a82f50c4be41dc869af
Madhushree Ray <madhushree@turbot.com>
2023-12-12 23:31:46 +0530

    Removed config schema

--- duo/connection_config.go
+++ duo/connection_config.go
@@ -2,25 +2,12 @@ package duo
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type duoConfig struct {
-	APIHostname    *string `cty:"api_hostname"`
-	IntegrationKey *string `cty:"integration_key"`
-	SecretKey      *string `cty:"secret_key"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"api_hostname": {
-		Type: schema.TypeString,
-	},
-	"integration_key": {
-		Type: schema.TypeString,
-	},
-	"secret_key": {
-		Type: schema.TypeString,
-	},
+	APIHostname    *string `hcl:"api_hostname"`
+	IntegrationKey *string `hcl:"integration_key"`
+	SecretKey      *string `hcl:"secret_key"`
 }
 
 func ConfigInstance() interface{} {
